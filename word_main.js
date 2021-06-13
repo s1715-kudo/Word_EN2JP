dba13970f09fac721a633d67af7dfa4e7bf49305
BuildTools <unconfigured@null.spigotmc.org>
2021-06-13 23:03:38 +0900

    display

--- word_main.js
+++ word_main.js
@@ -7,7 +7,7 @@ if(word!=""){
 
 	var display_data="<h1>"+word+"</h1>"
 	for(var d in r){
-		display_data+="<p>"+d+"："+r[d]+"</p>";
+		display_data+="<h2>"+d+"</h2><p>"+r[d]+"</p>";
 	}
 	document.getElementById("result_data").innerHTML=display_data;
 }
