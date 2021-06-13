5517dfa5e5d4a5b4bd07cf8f47dbf4b19f309f7e
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 01:12:45 +0900

    window

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var mydata=setKeyinit("dic","{}");
 function btnclick(){
 	var word=document.getElementById("input_word").value;
 	var wordurl="word.html?dic="+mydata+"&word="+word; 
-	window.open(wordurl,'_blank');
+	window.open(wordurl, null, 'width=500');
 }
 
 $("#input_word").keydown(function(event) {
