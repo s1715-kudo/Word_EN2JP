c9faa0e66718c6dc86e80c9d4a5301d961377350
BuildTools <unconfigured@null.spigotmc.org>
2021-06-13 23:24:45 +0900

    css

--- word_main.js
+++ word_main.js
@@ -7,7 +7,7 @@ if(word!=""){
 
 	var display_data="<h1>"+word+"</h1>"
 	for(var d in r){
-		display_data+="<h2>"+d+"</h2><p>"+r[d]+"</p>";
+		display_data+="<div class='result_div' id='result_"+d+"'><h2>"+d+"</h2><p class='description'>"+r[d]+"</p></div>";
 	}
 	document.getElementById("result_data").innerHTML=display_data;
 }
