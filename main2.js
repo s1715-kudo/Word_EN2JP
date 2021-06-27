2e60717680463ff4d52b0a8375f77b040006eb44
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 14:42:59 +0900

    word

---
+++ main2.js
@@ -0,0 +1,14 @@
+var mydata=setKeyinit("dic","{}");
+
+function btnclick(){
+	var word=document.getElementById("input_word").value;
+	var wordurl="word/word.html?dic="+mydata+"&word="+word;
+	var str="<iframe width='500' height='500' src='"+wordurl+"'></iframe>"
+	document.getElementById("result_data").innerHTML=str;
+}
+
+$("#input_word").keydown(function(event) {
+	if (event.key === "Enter"){
+		btnclick();
+	}
+});
