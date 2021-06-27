cc8fc8ecc2c061021e05ea39dc5eb6bed2536e46
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 15:36:36 +0900

    100%

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ var mydata=setKeyinit("dic","{}");
 function btnclick(){
 	var word=document.getElementById("input_word").value;
 	var wordurl="word/word.html?dic="+mydata+"&word="+word;
-	var str="<iframe width='500' height='500' src='"+wordurl+"'></iframe>"
+	var str="<iframe width='100%' height='500' src='"+wordurl+"'></iframe>"
 	document.getElementById("result_data").innerHTML=str;
 }
 
