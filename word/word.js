69f8161456f849db249360ac5e90744174bc4715
BuildTools <unconfigured@null.spigotmc.org>
2021-07-03 18:35:59 +0900

    cookie_test

--- word/word.js
+++ word/word.js
@@ -1,22 +1,6 @@
 var cookie_word_json=get_json('word_json',{});
 cookie_word_json=JSON.parse(JSON.stringify(cookie_word_json))
 
-
-global.zcookies = {
-	get: function(k) {
-		return msgpack.unpack(pako.inflateRaw(atob(Cookies.get(k))));
-	},
-	set: function(k, v, options) {
-		var x = Cookies.get(k);
-		var cookieSizeBeforeSave = document.cookie.length;
-		var y = btoa(String.fromCharCode.apply(null, pako.deflateRaw(msgpack.pack(v), {level: 9})));
-		Cookies.set(k, y, options);
-		if (x !== y && cookieSizeBeforeSave === document.cookie.length) {
-			throw new Error('zcookies: QUOTA Error!');
-		}
-	}
-};
-
 function get_word_json(){
 	var r={}
 	
