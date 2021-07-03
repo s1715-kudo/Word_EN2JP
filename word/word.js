9c30a638c61372dce81bf403d9bf79b0ac2067b9
BuildTools <unconfigured@null.spigotmc.org>
2021-07-03 16:09:18 +0900

    ver2.2

--- word/word.js
+++ word/word.js
@@ -1,6 +1,7 @@
 var cookie_word_json=Cookies.get('word_json');
 if (cookie_word_json===undefined||cookie_word_json==null)cookie_word_json="{}"
 cookie_word_json=JSON.parse(cookie_word_json)
+console.log(cookie_word_json)
 
 function getSearchWord(url){
 	var r={};
